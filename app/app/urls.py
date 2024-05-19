c31f451d01121b6288aab7c54740c7784b22db55
taehoondev030 <taehoondev030@gmail.com>
2024-05-19 11:51:34 +0900

    20240516

--- app/app/urls.py
+++ app/app/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
          SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'
     ),
+    path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls'))
 ]
